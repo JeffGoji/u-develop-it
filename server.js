26d4d29b891709821a4bb4798c374ecf04945ebe
Jeff Anderson-Lester <viperhq@hotmail.com>
2021-11-03 14:49:15 -0500

    added query for read,  delete, and create operations,

--- server.js
+++ server.js
@@ -1,4 +1,7 @@
 //Dependencies:
+//SQL:
+const mysql = require("mysql2");
+//Express:
 const express = require("express");
 
 //nodemon:
@@ -20,4 +23,57 @@ app.use((req, res) => {
 //Starts the express server on port 3001:
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
+});
+
+// code that will connect the application to the MySQL database:
+//Connect to database:
+const db = mysql.createConnection(
+  {
+    host: "localhost",
+    //your MySQL username, always is root:
+    user: "root",
+    //your MySQL password:
+    password: "password",
+    database: "election",
+  },
+  console.log("Connected to the election database")
+);
+//console should now say:
+//Connected to the election database
+//Server is running on port 3001
+
+//Database calls:
+//query the database to test the connection:
+// db.query(`SELECT * FROM candidates`, (err, rows) => {
+//   console.log(rows);
+// });
+
+//Create Query for Read Operation:
+//GET a single candidate:
+db.query(`SELECT * FROM candidates WHERE id = 1`, (err, row) => {
+  if (err) {
+    console.log(err);
+  }
+
+  console.log(row);
+});
+
+//DELETE a candidate:
+db.query(`DELETE FROM candidates WHERE id = ?`, 1, (err, result) => {
+  if (err) {
+    console.log(err);
+  }
+  console.log(result);
+});
+
+//Query for Create Operation:
+const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected)
+    VALUES (?,?,?,?)`;
+const params = [1, "Ronald", "Firbank", 1];
+
+db.query(sql, params, (err, result) => {
+  if (err) {
+    console.log(err);
+  }
+  console.log(result);
 });
